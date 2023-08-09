b1996911f1c75f50ee70ebb0eca68f204eba29f3
Arslan (Arsal) <marsalan@leeddev.io>
2023-08-09 10:55:43 +0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
-import { InvoiceGenerator } from "./screens/InvoiceGenerator/InvoiceGenerator";
-import RegistrationForm from "./screens/RegistrationForm/RegistrationForm.js";
 import React, { useState } from "react";
-import SignaturePopup from "./screens/SignaturePopup";
+
 import MainForm from "./screens/MainForm/MainForm.js";
 function App() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -17,16 +15,6 @@ function App() {
 
   return (
     <div>
-      {/* <SignaturePopup /> */}
-
-      {/* <div className="regPopup">
-        <h1>Registration Form Popup</h1>
-        <button onClick={openPopup}>Open Registration Form</button>
-
-        {isPopupOpen && <RegistrationForm onClose={closePopup} />}
-      </div> */}
-
-      {/* <InvoiceGenerator /> */}
       <MainForm />
     </div>
   );
